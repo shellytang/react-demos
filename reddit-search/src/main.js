e42035228d041d27ebe06f46bdebbc26ed33af6f
Shelly Tang <shellytang@gmail.com>
2017-07-26 13:34:00 -0700

    completed form and created search results component

--- reddit-search/src/main.js
+++ reddit-search/src/main.js
@@ -5,21 +5,58 @@ import superagent from 'superagent';
 
 const API_URL = 'http://www.reddit.com/r';
 //* App - manage app state
-//  *SearchForm -- collect user input
+//  *SearchForm -- collect user input and call handleSearch function
 //  *SearchResultsList -- display reddit articles
 
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value,
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSearch(this.state.searchText);
   }
   render() {
-    return <p>cool</p>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>{this.props.title}</label>
+        <input
+          type='text'
+          onChange={this.handleChange}
+          value={this.state.searchText}
+        />
+        <button type='submit'>Search</button>
+      </form>
+    );
   }
 }
-
+// should receive an array of reddit articles through props
 class SearchResultsList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return <p>hi</p>;
+    let articles = this.props.articles || [];
+    console.log('articles', articles);
+    return (
+      <ul>
+        {articles.map((item, i) =>
+          <li key={i}>
+            <a href={item.data.url}>{item.data.title}</a>
+          </li>
+        )}
+      </ul>
+    );
   }
 }
 //holds all application state
@@ -42,7 +79,7 @@ class App extends React.Component {
       .then(res => {
         console.log('request success', res);
         this.setState({
-          results: res.body,
+          results: res.body.data.children,
           searchErrorMessage: null,
         });
       })
@@ -59,8 +96,11 @@ class App extends React.Component {
     return (
       <main>
         <h1>cool beans</h1>
-        <SearchForm />
-        <SearchResultsList />
+        <SearchForm
+          title='Reddit board'
+          handleSearch={this.redditBoardFetch}
+        />
+        <SearchResultsList articles={this.state.results || []} />
       </main>
     );
   }
