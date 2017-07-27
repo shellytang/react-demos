52f79d31b2a57eabf8973dff22a415b43142f1d5
Shelly Tang <shellytang@gmail.com>
2017-07-27 14:38:53 -0700

    unit testing with jest for expense create form

---
+++ budget-tracker/src/__test__/expense-create-form.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ExpenseCreateForm from '../component/expense-create-form';
+
+describe('testing ExpenseCreateForm', () => {
+  test('it should have correct default state', () => {
+    let wrapper = shallow(<ExpenseCreateForm handleExpenseCreate={() => {}} />);
+    expect(wrapper.state('title')).toBe('');
+    expect(wrapper.state('price')).toBe(0);
+  });
+  test('expect submit to invoke handleExpenseCreate', () => {
+    let expenseCreate = (expense) => {
+      expect(expense.title).toEqual('');
+      expect(expense.price).toEqual(0);
+    };
+    let wrapper = shallow(<ExpenseCreateForm handleExpenseCreate={expenseCreate} />);
+    wrapper.find('form').simulate('click');
+  });
+});
+
+// describe('testing cool stuff', () => {
+//   test('it should pass', () =>{
+//     expect(true).toEqual(true);
+//   });
+// });
