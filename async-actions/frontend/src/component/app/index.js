792f194839e4463f924163d20424530fd92a6084
Shelly Tang <shellytang@gmail.com>
2017-08-10 14:22:16 -0700

    initial src directories and files

--- async-actions/frontend/src/component/app/index.js
+++ async-actions/frontend/src/component/app/index.js
@@ -1,1 +1,18 @@
-import React from 'react'; 
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <BrowserRouter>
+          <main>
+            <h1>cool beans</h1>
+          </main>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
