ec253ae636b17b93571d4db23380272650ffe335
Shelly Tang <shellytang@gmail.com>
2017-08-10 14:45:07 -0700

    add the store to the App component

--- async-actions/frontend/src/component/app/index.js
+++ async-actions/frontend/src/component/app/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import appStoreCreate from '../../lib/app-store-create.js';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+const store = appStoreCreate();
 
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <BrowserRouter>
-          <main>
-            <h1>cool beans</h1>
-          </main>
-        </BrowserRouter>
+        <Provider sotre={store}>
+          <BrowserRouter>
+            <main>
+              <h1>cool beans</h1>
+            </main>
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
